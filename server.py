cbfa3424cfc574c6336db6d8068b67fa1c83db35
m477h411-M <m477h411@gmail.com>
2026-09-10 20:34:28 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # ai-visualizer: give your AI agent a face.
-# Copyright (C) 2026 Jared Rhodenizer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
